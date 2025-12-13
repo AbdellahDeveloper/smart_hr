d9039ac7786447a36c2f4eae427b48e451bf1380
Abdellah Elidrissi <brahim2.elidrissi2@gmail.com>
2025-12-13 12:21:35 +0100

    Update page.tsx

--- app/auth/verify-email/page.tsx
+++ app/auth/verify-email/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import { VerifyEmailForm } from "@/app/auth/_components/verify-email-form";
 
 export default function VerifyEmailPage() {
-    return <VerifyEmailForm />;
+    return (
+        <Suspense fallback={<div className="mx-auto space-y-4 sm:w-sm text-center">Loading...</div>}>
+            <VerifyEmailForm />
+        </Suspense>
+    );
 }
